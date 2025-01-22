cc2944a6317e14cb8db55d38864767ea292f0617
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-01-22 17:42:45 +0000

    Added the operation validation code

--- number_game.py
+++ number_game.py
@@ -1,5 +1,21 @@
+import random
+
 def main():
-    ...
+    n = int(input("Give the upper value: "))
+    rando_number = random.randint(1,n)
+
+    while True:
+        try:
+            inp = int(input("Put your guess: "))
+            if inp == rando_number:
+                print("Congrats 🎉! You guessed it right")
+                break
+            elif inp < rando_number:
+                print("Oh You are close! A hint, Try a BIGGER number!")
+            else:
+                print("Oh You are close! A hint,Try a SMALLER number!")
+        except ValueError:
+            print("Invalid input! Enter an integer")
 
 
 if __name__=="__main__":
