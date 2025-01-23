aa2235b886956ebd03500035d6af962252f87b7e
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-01-23 14:31:59 +0000

    Organized scripts into projects folder and added new DSA code

--- dsa/1.py
+++ dsa/1.py
@@ -1,6 +1,14 @@
 #Given an array with some integer type values. 
 # Write a python script to sort array values
 
-arr = [45,3,6,86,32,5]
-arr.sort()
+from array import *
+
+arr = array('i',[45,3,6,86,32,5])
+print("Original Array:", arr)
+arr = array('i', sorted(arr))
 print("Sorted array: ", arr)
+
+
+#arr = [45,3,6,86,32,5]
+#arr.sort()
+#print("Sorted array: ", arr)
