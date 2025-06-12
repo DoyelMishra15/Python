0e2f3a783bcea31dcb67452f592379464b9a6d9e
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-06-12 11:14:35 +0000

    Done

--- flask_practice/app.py
+++ flask_practice/app.py
@@ -2,10 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/potato')
 def welcome():
     return "This is my first Flask app"
 
+@app.route('/')
+def wroot():
+    return "This is my root Flask app"
 
+@app.route('/bob')
+def bob():
+    return "This is my first Flask app"
 
 app.run()
