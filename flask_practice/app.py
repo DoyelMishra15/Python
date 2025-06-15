12177ae280581fdb268a133530057149a3430ef4
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-06-15 07:55:37 +0000

    Done

--- flask_practice/app.py
+++ flask_practice/app.py
@@ -2,23 +2,8 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/potato')
-def welcome():
-    return "This is my first Flask app"
-
 @app.route('/')
 def wroot():
     return render_template("index.html")
-
-@app.route('/bob')
-def bob():
-    return "This is bob Flask app"
-
-@app.route('/method',methods=['GET','POST'])
-def method():
-    if request.method=='POST':
-        return "Post request"
-    else:
-        return "maybe using a Get request"
 
 app.run()
