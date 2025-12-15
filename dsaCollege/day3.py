8492631e0a79b89c8f16f7fbc5adf871cd357d6a
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-15 05:22:10 +0000

    Your commit message here

--- dsaCollege/day3.py
+++ dsaCollege/day3.py
@@ -18,3 +18,68 @@ for i in range(1,10):
 for i in range(10,0,-2):
     print(i)
 
+##first 10 natural num
+
+for i in range(1,11):
+    print(i, end=" ")
+
+##first 10 natural num
+
+for i in range(1,11):
+    print(i, end=" ")
+    if i==5:
+        break
+
+for i in range(1,11):
+    if i==5 or i==7:
+        continue
+    print(i, end=" ")
+
+for i in range(1,11):
+    print(i)
+    #if i==7:
+        #break
+else:
+    print("xhfhr")
+
+# else block is executed if loop is run without breaking
+
+#Sum of First 10 Natural Numbers
+sum=0#flag variable, where we make an assumption
+for i in range(1,11):
+    sum+=i
+print(sum)
+
+x,y=15,17
+print(f"sum of x and y: {x+y}")
+
+x,y=15,17
+print("sum of x and y: {}".format(x+y))
+
+x=int(input())
+for i in range(1,x+1):
+    print(f"Number is : {i} and cube of the {i} is :{i**3}")
+
+x=int(input())
+print()
+for i in range(1,11):
+    print(f"{x} X {i} = {x*i}")
+
+# Factorial Calculation
+x=int(input())
+print()
+fact=1
+for i in range(1,x+1):
+    fact*=i
+print(f"The Factorial of {x} is: {fact}")
+
+#  Harmonic Series and Their Sum
+
+
+#Sum of Series [9 + 99 + 999 + …]
+
+
+#Sum of Series [1 - X²/2! + X⁴/4! - …]
+
+
+
