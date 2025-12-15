0c5582859dd740d179e39c773f1917b10793aea4
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-15 05:18:49 +0000

    Your commit message here

--- dsaCollege/day3.py
+++ dsaCollege/day3.py
@@ -5,3 +5,16 @@
 
 for i in range(1,10):
     print(i)
+
+for i in range(1,10,1):
+    print(i)
+
+for i in range(1,11,3):
+    print(i)
+
+for i in range(1,10):
+    print(i, end=" ")
+
+for i in range(10,0,-2):
+    print(i)
+
