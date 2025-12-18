da857f74c952c6f626801513a279e66d3b874232
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-18 05:02:38 +0000

    done

--- dsaCollege/day6.py
+++ dsaCollege/day6.py
@@ -23,3 +23,36 @@ def add():
 add()
 
 #argument types in python
+
+#1.Positional Arguments
+
+#Passed by order. Python matches values to parameters based on their position.
+def greet(name, age):
+    print(f"Hello {name}, you are {age} years old")
+
+greet("Doyel", 21)
+
+#2️⃣ Keyword Arguments: Passed using parameter names, order doesn’t matter.
+def greet(name, age):
+    print(f"Hello {name}, you are {age} years old")
+
+greet(age=21, name="Doyel")
+# Output: Hello Doyel, you are 21 years old
+#28 leetcode
+'''class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle not in haystack:
+            return -1
+        else:
+            for i in range(len(haystack)):
+                if needle==haystack[i:len(needle)+i]:
+                    return i'''
+
+
+#2nd way
+'''class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1'''
