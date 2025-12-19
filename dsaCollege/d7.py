0adc0cd723e2dd0d296e92d55969d48e9b2af546
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-19 06:06:48 +0000

    done

--- dsaCollege/d7.py
+++ dsaCollege/d7.py
@@ -48,3 +48,13 @@
         return max_water'''
 
 #26
+'''class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k=1
+        i=1
+        for i in range(1,len(nums)):
+            if nums[i]!=nums[i-1]:
+                nums[k]=nums[i]
+                k+=1
+        return k
+'''
