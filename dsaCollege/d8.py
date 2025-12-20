cd2396f868d4ae86334079d696e2721d00c18c79
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-20 05:15:33 +0000

    done

--- dsaCollege/d8.py
+++ dsaCollege/d8.py
@@ -24,3 +24,11 @@
                 nums1[k]=nums2[j]
                 j+=1
                 k+=1'''
+'''class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(n):
+            nums1[m+i]=nums2[i]
+        nums1.sort()'''
