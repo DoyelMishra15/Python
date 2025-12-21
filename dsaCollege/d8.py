4658d038342a8f260c574eeb482a9de0b2609397
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-21 16:20:39 +0000

    fyf

--- dsaCollege/d8.py
+++ dsaCollege/d8.py
@@ -55,7 +55,18 @@
 
 #1423
 
-
+'''class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        if k==len(cardPoints):
+            return sum(cardPoints[:])
+        else:
+            lsum = sum(cardPoints[0:k])
+            rsum, s=0,lsum
+            for i in range(k-1,-1,-1):
+                lsum = lsum - cardPoints[i]
+                rsum= rsum + cardPoints[len(cardPoints) - k + i]
+                s = max(s,lsum+rsum)
+            return s''''''
 
 
 
