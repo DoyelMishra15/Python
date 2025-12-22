a6401cbc8124de8b1bf9d01c19bbdfe54b8dbb9d
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-22 05:27:25 +0000

    d9

--- dsaCollege/d9.py
+++ dsaCollege/d9.py
@@ -35,5 +35,14 @@ print(x)
             else:
                 d[nums[i]]=i'''
 
+#217 
+'''class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(0,len(nums)-1):
+            if nums[i]==nums[i+1]:
+                return True
+        return False'''
+
 #used in duplicates, 
 # set operations like union, intersection, difference,only then use set
