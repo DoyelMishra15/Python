dd35207e3a09283b997ce7a10426d63af03337c6
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-23 04:43:58 +0000

    hfbhfhf

--- dsaCollege/d10.py
+++ dsaCollege/d10.py
@@ -5,8 +5,13 @@ class Form:
     clg_name="dsaCollege"
     clg_mob=9999999999
 
+    def __init__(self,name,p,a=18):
+        self.name=name
+        self.phone=p
+        self.age=a
+        
 
-o1 = Form()
-o2 = Form()
-print(o1.clg_name)
-print(o1.clg_mob)
+o1 = Form("Aria","1234567890",20)
+o2 = Form("Bob","0987654321")
+print(o1.phone)
+print(o2.age)
