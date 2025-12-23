53cdacbe7373b657577b221a9fddb5fae40477f3
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-23 05:21:04 +0000

    hfbhfhf

--- dsaCollege/d10_2.py
+++ dsaCollege/d10_2.py
@@ -10,10 +10,18 @@ n1 = Node(10)
 #n1=Node(10,None)
 n2 = Node(20)
 n3 = Node(30)
+n4 = Node(40)
+n5 = Node(50)
 
+print(n1.next)
 n1.next=n2
+print(n1.next)
 n2.next=n3
+n3.next=n4
+n4.next=n5
 
 print(n1.data)
 print(n1.next.data)
 print(n1.next.next.data)
+print(n1.next.next.next.data)
+print(n1.next.next.next.next.data)
