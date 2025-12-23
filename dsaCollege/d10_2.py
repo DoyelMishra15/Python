3d85ad9509e63a1a1273b16bf15b7d4c1856c65e
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-23 05:53:35 +0000

    hfbhfhf

--- dsaCollege/d10_2.py
+++ dsaCollege/d10_2.py
@@ -1,35 +1,36 @@
-class Node:
-    #def __init__(self,data,next): if passing next node
+# class Node:
+#     #def __init__(self,data,next): if passing next node
     
-    def __init__(self,data):
-        self.data = data
-        #self.next = next
-        #next ki jagah None
-        self.next = None
+#     def __init__(self,data):
+#         self.data = data
+#         #self.next = next
+#         #next ki jagah None
+#         self.next = None
     
-    def display(self):
-        t = n1 #n3
-        while t:
-            print(t.data, end="->")
-            t = t.next
-        print()
+#     def display(self):
+#         t = n1 #n3
+#         while t:
+#             print(t.data, end="->")
+#             t = t.next
+#         print()
+    
 
-n1 = Node(10)
-#n1=Node(10,None)
-n2 = Node(20)
-n3 = Node(30)
-n4 = Node(40)
-n5 = Node(50)
+# n1 = Node(10)
+# #n1=Node(10,None)
+# n2 = Node(20)
+# n3 = Node(30)
+# n4 = Node(40)
+# n5 = Node(50)
 
-#print(n1.next)
+# #print(n1.next)
 
-n1.next=n2
+# n1.next=n2
 
-#print(n1.next)
+# #print(n1.next)
 
-n2.next=n3
-n3.next=n4
-n4.next=n5
+# n2.next=n3
+# n3.next=n4
+# n4.next=n5
 
 # print(n1.data)
 # print(n1.next.data)
@@ -37,4 +38,40 @@ n4.next=n5
 # print(n1.next.next.next.data)
 # print(n1.next.next.next.next.data)
 
-n1.display()
+# n1.display()
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+    
+    def length(self):
+        t=n1
+        c=0
+        while t:
+            c+=1
+            t=t.next
+        
+        
+n1 = Node(10)
+n2 = Node(20)
+n3 = Node(30)
+n4 = Node(40)
+n5 = Node(50)
+
+n1.next=n2
+n2.next=n3
+n3.next=n4
+n4.next=n5
+
+# 1. **Find Length of a Linked List**
+
+# 2. **Find Sum of All Elements**
+# 3. **Find Maximum Element**
+# 4. **Find Minimum Element**
+# 5. **Search an Element in Linked List**
+# 6. **Count Occurrences of a Given Element**
+# 7. **Find Average of Linked List Elements**
+# 8. **Print Linked List in Reverse Order**
+# 9. **Find Middle Element of Linked List**
+# 10. **Check if Linked List is Empty**
