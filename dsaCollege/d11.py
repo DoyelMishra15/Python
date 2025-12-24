4c4526c253d4092c8606521d45bfdcf7c5836637
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-24 04:33:52 +0000

    hh

--- dsaCollege/d11.py
+++ dsaCollege/d11.py
@@ -1,3 +1,14 @@
 #CRUD or CURD Operations.....
 #create, read, update, delete
 
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+    def display(self):
+        t = n1 #n3
+        while t:
+           print(t.data, end="->")
+           t = t.next
+        print()
+
