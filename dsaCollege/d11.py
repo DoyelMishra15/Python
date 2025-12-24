73e6a32afdeccb96fe3fb22a4499babfcfb41075
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-24 04:38:59 +0000

    hh

--- dsaCollege/d11.py
+++ dsaCollege/d11.py
@@ -5,10 +5,17 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
-    def display(self):
-        t = n1 #n3
-        while t:
-           print(t.data, end="->")
-           t = t.next
-        print()
 
+class SLL:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def IAS(self):#insert at start
+        pass
+
+    def IAE(self):#insert at end
+        pass
+    
+    def IAM(self):#insert at middle
+        pass
