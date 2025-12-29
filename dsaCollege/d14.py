ab45c6b6d62b1728700df440f7f31ea0511c380f
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-29 05:16:25 +0000

    done

--- dsaCollege/d14.py
+++ dsaCollege/d14.py
@@ -33,3 +33,20 @@ class Solution:
         return False'''
 
 #202
+
+'''class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()  # To detect cycles
+
+        while n != 1 and n not in seen:
+            seen.add(n)
+            t = n
+            s = 0
+            while t > 0:
+                d = t % 10          
+                s += d**2           
+                t //= 10            
+            n = s                   
+        return n == 1'''
+
+#19
