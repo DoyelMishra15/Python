c56e30f77b5e46826eac40f0b2c05ba067fc7002
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-29 05:01:16 +0000

    done

--- dsaCollege/d14.py
+++ dsaCollege/d14.py
@@ -18,15 +18,18 @@ class Solution:
 #141
 '''# Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-            f=s=head
-            while f and f.next:
-                f=f.next.next
-                s=s.next
-                if f==s:
-                    return True
-            return False'''
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        f=s=head
+        while f and f.next:
+            f=f.next.next
+            s=s.next
+            if f==s:
+                return True
+        return False'''
+
+#202
