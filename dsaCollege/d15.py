8a9d7e94130409f7c87715e5c1b92de08a6dc7fa
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-30 05:28:56 +0000

    fff

--- dsaCollege/d15.py
+++ dsaCollege/d15.py
@@ -64,3 +64,19 @@ class Solution:
             curr = next_node       
         
         return prev'''
+
+'''# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p,n = None,None
+        c = head
+        while c:
+            n=c.next
+            c.next=p
+            p=c
+            c=n
+        return p'''
