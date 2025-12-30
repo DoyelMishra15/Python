a6fd82625b791e2f9b0428172ca9a8c3abcadde4
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-12-30 04:48:44 +0000

    fff

--- dsaCollege/d15.py
+++ dsaCollege/d15.py
@@ -30,3 +30,17 @@ class Solution:
             h1 = h1.next
             h2 = h2.next  
         return None'''
+
+'''# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        p, q = headA, headB
+        while p!=q:
+            p = p.next if p else headB
+            q = q.next if q else headA
+        return p'''
