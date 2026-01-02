bbe4bf85c8beb288033ab9aba0db8a1587587aea
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-02 04:32:47 +0000

    fdff

---
+++ dsaCollege/d16.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+class DLL:
+    def __init__(self):
+        self.head=None
+        self.tail=None
