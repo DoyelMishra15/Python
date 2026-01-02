5f89cdb5e259f55b76a7fab3064bdcbbebdfd923
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-02 04:38:28 +0000

    fdff

--- dsaCollege/d16.py
+++ dsaCollege/d16.py
@@ -2,8 +2,30 @@ class Node:
     def __init__(self,data):
         self.data=data
         self.next=None
-        self.prev=None
-class DLL:
+class SLL:
     def __init__(self):
         self.head=None
         self.tail=None
+    def IAS(self,d):
+        nn=Node(d)
+        if not self.head:
+            self.head=nn
+            self.tail=nn
+        else:
+            nn.next=self.head
+            self.head=nn
+    def IAE(self,d):
+        nn=Node(d)
+        if not self.head:
+            self.head=nn
+            self.tail=nn
+        else:
+            self.tail.next=nn
+            self.tail=nn
+    def display(self):
+        t=self.head
+        while t:
+            print(t.data,end="->")
+            t=t.next
+        print()
+        
