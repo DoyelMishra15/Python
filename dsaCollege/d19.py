246ec37549a6676476e771e16bc4c026575c5c27
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-06 04:39:37 +0000

    donr

---
+++ dsaCollege/d19.py
@@ -0,0 +1,16 @@
+m=[[1,2,3],[4,5,6],[7,8,9]]
+for i in range(3):
+    for j in range(3):
+        if i+j==2:
+            print(m[i][j], end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+    
+for i in range(3):
+    for j in range(3):
+        if i==j:
+            print(m[i][j], end=" ")
+        else:
+            print(" ", end=" ")
+    print()
