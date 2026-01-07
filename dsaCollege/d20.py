5e7f28b0b2b74b5af0ddc6aa6225f8565942c0bd
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-07 04:41:51 +0000

    jhf

--- dsaCollege/d20.py
+++ dsaCollege/d20.py
@@ -10,6 +10,21 @@
 
 #73
 
-
+'''class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l=[]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j]==0:
+                    l.append((i,j))
+        
+        for i,j in l:
+            for col in range(len(matrix[0])):
+                matrix[i][col]=0
+            for row in range(len(matrix)):
+                matrix[row][j]=0'''
 
 #spiral matrix
