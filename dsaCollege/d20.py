28a0765f4e5024da65b42502a255d14f9c5c125c
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-07 04:25:09 +0000

    jhf

--- dsaCollege/d20.py
+++ dsaCollege/d20.py
@@ -1,5 +1,12 @@
 #1351
-
+'''class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        c=0
+        for i in grid:
+            for j in i:
+                if j < 0:
+                    c+=1
+        return c'''
 
 #73
 
