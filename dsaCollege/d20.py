33f3b70840f4332e8c33b98e667587d83d54368b
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-07 05:04:23 +0000

    jhf

--- dsaCollege/d20.py
+++ dsaCollege/d20.py
@@ -51,4 +51,14 @@
                 matrix[i][j],matrix[j][i]= matrix[j][i],matrix[i][j]
             matrix[i]=matrix[i][::-1]'''
 
+#867
+'''class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        result = [[0] * rows for _ in range(cols)]
+        for i in range(rows):
+            for j in range(cols):
+                result[j][i] = matrix[i][j]
+        return result'''
 #spiral matrix
