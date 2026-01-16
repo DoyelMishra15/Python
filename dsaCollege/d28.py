92eae743f725fb8e659ad53d4c30b5f003071a75
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-16 04:46:04 +0000

    hhud

--- dsaCollege/d28.py
+++ dsaCollege/d28.py
@@ -7,3 +7,12 @@ for i in range(len(arr)-1,-1,-1):
       arr[j],arr[j+1]=arr[j+1],arr[j]
 print(arr)
 
+#905, did it myself, not of course
+
+'''class Solution:
+    def sortArrayByParity(self, arr: List[int]) -> List[int]:
+        for i in range(len(arr)-1,-1,-1):
+            for j in range(i):
+                if arr[j]%2>arr[j+1]%2:
+                    arr[j],arr[j+1]=arr[j+1],arr[j]
+        return arr'''
