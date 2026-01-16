0c122079f36dcc07f1e08b896943070301b5549d
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-16 04:49:56 +0000

    hhud

--- dsaCollege/d28.py
+++ dsaCollege/d28.py
@@ -1,11 +1,18 @@
 #bubble sort
  
 arr= [5,4,3,2,1]
-for i in range(len(arr)-1,-1,-1):
+'''for i in range(len(arr)-1,-1,-1):
   for j in range(i):
     if arr[j]>arr[j+1]:
       arr[j],arr[j+1]=arr[j+1],arr[j]
+print(arr)'''
+
+for i in range(len(arr)):
+  for j in range(len(arr)-i-1):
+    if arr[j]>arr[j+1]:
+      arr[j],arr[j+1]=arr[j+1],arr[j]
 print(arr)
+
 
 #905, did it myself, not of course
 
