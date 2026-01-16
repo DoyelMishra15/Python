a3a5661ad5b9d3c6eb5bc206896e6c1d518ffcde
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-16 04:51:53 +0000

    hhud

--- dsaCollege/d28.py
+++ dsaCollege/d28.py
@@ -14,10 +14,10 @@ print(arr)'''
 print(arr)'''
 
 
-for i in range(len(arr)):
+for i in range(len(arr)-1):
   for j in range(1,len(arr)-i):
-    if arr[j]>arr[j+1]:
-      arr[j],arr[j+1]=arr[j+1],arr[j]
+    if arr[j]<arr[j-1]:
+      arr[j],arr[j-1]=arr[j-1],arr[j]
 print(arr)
 
 
