d8e22e7de194375d514de416296689eaa35679dc
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-22 05:19:01 +0000

    done

--- dsaCollege/d34.py
+++ dsaCollege/d34.py
@@ -13,3 +13,22 @@ class Solution:
         right = self.maxDepth(root.right)
         return 1 + max(left, right)
         '''
+
+#98
+
+'''# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def i(r,res):
+            if r:
+                i(r.left,res)
+                res.append(r.val)
+                i(r.right,res)
+            return res
+        a=i(root,[])
+        return a==sorted(a) and len(a)==len(set(a))'''
