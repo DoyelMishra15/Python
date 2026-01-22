88a5a7a7c1e0fffdb747b59c8726d838a06a998f
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-22 05:56:54 +0000

    done

--- dsaCollege/d34.py
+++ dsaCollege/d34.py
@@ -32,3 +32,33 @@ class Solution:
             return res
         a=i(root,[])
         return a==sorted(a) and len(a)==len(set(a))'''
+
+#116
+
+'''
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        leftmost = root
+        
+        while leftmost.left:
+            curr = leftmost
+            while curr:
+                curr.left.next = curr.right
+                if curr.next:
+                    curr.right.next = curr.next.left
+                curr = curr.next
+            leftmost = leftmost.left
+        return root
+        '''
