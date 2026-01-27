b43e02446dd4d0dfaeaf393503cfc2fa86a402dc
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-01-27 04:58:28 +0000

    done

--- dsaCollege/bdfs.py
+++ dsaCollege/bdfs.py
@@ -8,4 +8,19 @@ g = {
 }
 
 #bfs
+def bfs(q, v):
+    if q == []:
+        return
 
+    x = q[0]
+    print(x, end=" ")
+    q = q[1:]
+
+    for i in g[x]:
+        if i not in v:
+            v.append(i)
+            q.append(i)
+
+    bfs(q, v)
+
+bfs(["a"], ["a"])
